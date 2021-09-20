6882aa720c4ad9a6560a42c13f8205861816ca95
Chort <Chort@DESKTOP-TI13LL6>
2021-09-21 00:23:07 +0400

    Добавьте файлы проекта.

---
+++ WpfApp4/Person/ManWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для ManWindow.xaml
+    /// </summary>
+    public partial class ManWindow : Window
+    {
+        public ManWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Story1button_Click(object sender, RoutedEventArgs e)
+        {
+            StoryMan storyMan = new StoryMan();
+            storyMan.Show();
+            this.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ChousePerson chousePerson = new ChousePerson();
+            chousePerson.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp4/Person/WomanWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Person
+{
+    /// <summary>
+    /// Логика взаимодействия для WomanWindow.xaml
+    /// </summary>
+    public partial class WomanWindow : Window
+    {
+        public WomanWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Story2button_Click(object sender, RoutedEventArgs e)
+        {
+            WomanStory womanStory = new WomanStory();
+            womanStory.Show();
+            this.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ChousePerson chouse = new ChousePerson();
+            chouse.Show();
+            this.Close();
+        }
+    }
+}
