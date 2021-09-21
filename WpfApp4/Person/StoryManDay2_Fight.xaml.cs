0329cb6d8d1c7cec2762743764d0d0bbb3df50a7
0NE <0netw08778@gmail.com>
2021-09-22 00:28:37 +0400

    Добавлен второй день и бой

---
+++ WpfApp4/Person/StoryManDay2_Fight.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Person
+{
+    /// <summary>
+    /// Логика взаимодействия для StoryManDay2_Fight.xaml
+    /// </summary>
+    public partial class StoryManDay2_Fight : Window
+    {
+        public StoryManDay2_Fight()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            StoryManDay2Napadenie storyManDay2Napadenie = new StoryManDay2Napadenie();
+            storyManDay2Napadenie.Show();
+            this.Close();
+        }
+    }
+}
